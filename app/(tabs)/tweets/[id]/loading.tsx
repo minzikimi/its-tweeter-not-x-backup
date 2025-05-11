eb0ccd54b8fc1f3085c6a70e3b7daa98fe77d5b5
minzikimi <minji.kim@hyperisland.se>
2025-05-11 19:36:52 +0200

    change background on loading page on tweet detail

--- app/(tabs)/tweets/[id]/loading.tsx
+++ app/(tabs)/tweets/[id]/loading.tsx
@@ -1,12 +1,11 @@
 export default function Loading() {
-    return (
-      <div className="p-4">
-        <div className="h-8 w-2/3 bg-neutral-700 rounded mb-2 animate-pulse" />
-        <div className="h-4 w-1/3 bg-neutral-800 rounded mb-8 animate-pulse" />
-        <div className="mt-4">
-          <div className="px-4 py-2 bg-neutral-700 rounded w-24 h-10 animate-pulse" />
-        </div>
+  return (
+    <div className="p-4 bg-black min-h-screen">
+      <div className="h-8 w-2/3 bg-neutral-700 rounded mb-2 animate-pulse" />
+      <div className="h-4 w-1/3 bg-neutral-800 rounded mb-8 animate-pulse" />
+      <div className="mt-4">
+        <div className="px-4 py-2 bg-neutral-700 rounded w-24 h-10 animate-pulse" />
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
