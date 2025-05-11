04438f236ec51b2d4c8faa27b7447b1256763ed4
minzikimi <minji.kim@hyperisland.se>
2025-05-11 19:48:07 +0200

    correct params type

--- app/users/[username]/edit/page.tsx
+++ app/users/[username]/edit/page.tsx
@@ -3,11 +3,11 @@ import getSession from "@/app/lib/session";
 import { notFound } from "next/navigation";
 import EditProfileForm from "./edit-profile";
 
-export default async function EditProfilePage({ params }: { params: Promise<{ username: string }> }) {
+export default async function EditProfilePage({ params }: { params: { username: string } }) {
   const session = await getSession();
   if (!session?.id) notFound();
 
-  const { username } = await params;
+  const { username } = params;
 
   const user = await db.user.findUnique({
     where: { username },
@@ -15,7 +15,7 @@ export default async function EditProfilePage({ params }: { params: Promise<{ us
   });
 
   if (!user) notFound();
-  if (session.id !== user.id) notFound(); // id로 비교!
+  if (session.id !== user.id) notFound();
 
   return <EditProfileForm user={{
     ...user,
